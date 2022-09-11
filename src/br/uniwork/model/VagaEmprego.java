d8aa6c4ee43e19b4c9993b7bcca38e70261b3964
Mateus-Cabral <RM93000@fiap.com.br>
2022-09-11 11:40:53 -0300

    Alterando diretório e nome de classe

--- src/br/uniwork/model/VagaEmprego.java
+++ src/br/uniwork/model/VagaEmprego.java
@@ -1,4 +1,4 @@
-package br.uniwork.controller;
+package br.uniwork.model;
 /**
  * Classe que abstrai uma proposta de emprego dentro do sistema.
  * @author mateus-cabral
@@ -6,12 +6,12 @@ package br.uniwork.controller;
  */
 import java.util.ArrayList;
 
-public class PropostaEmprego {
+public class VagaEmprego {
 	
 	/**
 	 * Id da proposta no banco de dados
 	 */
-	private int idProposta;
+	private int idVaga;
 	
 	/**
 	 * Id da empresa no banco de dados
@@ -40,23 +40,26 @@ public class PropostaEmprego {
 	
 	/**
 	 * Lista de habilidades requisitadas
+	 * @deprecated
 	 */
 	private ArrayList<Integer> idHabilidade = new ArrayList<Integer>();
 	
 	/**
 	 * Lista de candidatos candidatados
+	 * @deprecated
 	 */
 	private ArrayList<Integer> idCandidatos = new ArrayList<Integer>();
 	
 	/**
 	 * Verifica se a proposta está ativa
+	 * @deprecated
 	 */
 	private boolean status;
 	
 	/**
 	 * Construtor vazio
 	 */
-	public PropostaEmprego() {
+	public VagaEmprego() {
 		
 	}
 	
@@ -70,8 +73,8 @@ public class PropostaEmprego {
 	 * @param local String
 	 * @param skills ArrayList<Integer>
 	 */
-	public PropostaEmprego(int idProposta, int idEmpresa, String nomeCargo, String descritivo, double salario, String local, ArrayList<Integer> skills) {
-		this.setIdProposta(idProposta);
+	public VagaEmprego(int idVaga, int idEmpresa, String nomeCargo, String descritivo, double salario, String local, ArrayList<Integer> skills) {
+		this.setIdVaga(idVaga);
 		this.setIdEmpresa(idEmpresa);
 		this.setNomeCargo(nomeCargo);
 		this.setDescritivo(descritivo);
@@ -91,104 +94,134 @@ public class PropostaEmprego {
 	public void fecharProposta() {
 		this.setStatus(false);
 	}
-	
+
 	
 	//Getters e Setters
-	public int getIdProposta() {
-		return idProposta;
+	
+	/**
+	 * @return the idVaga
+	 */
+	public int getIdVaga() {
+		return idVaga;
 	}
 
-
-	public void setIdProposta(int idProposta) {
-		this.idProposta = idProposta;
+	/**
+	 * @param idVaga the idVaga to set
+	 */
+	public void setIdVaga(int idVaga) {
+		this.idVaga = idVaga;
 	}
 
-
+	/**
+	 * @return the idEmpresa
+	 */
 	public int getIdEmpresa() {
 		return idEmpresa;
 	}
 
-
+	/**
+	 * @param idEmpresa the idEmpresa to set
+	 */
 	public void setIdEmpresa(int idEmpresa) {
 		this.idEmpresa = idEmpresa;
 	}
 
-
+	/**
+	 * @return the nomeCargo
+	 */
 	public String getNomeCargo() {
 		return nomeCargo;
 	}
 
-
+	/**
+	 * @param nomeCargo the nomeCargo to set
+	 */
 	public void setNomeCargo(String nomeCargo) {
 		this.nomeCargo = nomeCargo;
 	}
 
-
+	/**
+	 * @return the descritivo
+	 */
 	public String getDescritivo() {
 		return descritivo;
 	}
 
-
+	/**
+	 * @param descritivo the descritivo to set
+	 */
 	public void setDescritivo(String descritivo) {
 		this.descritivo = descritivo;
 	}
 
-
+	/**
+	 * @return the salario
+	 */
 	public double getSalario() {
 		return salario;
 	}
 
-
+	/**
+	 * @param salario the salario to set
+	 */
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
 
-
+	/**
+	 * @return the local
+	 */
 	public String getLocal() {
 		return local;
 	}
 
-
+	/**
+	 * @param local the local to set
+	 */
 	public void setLocal(String local) {
 		this.local = local;
 	}
 
-
+	/**
+	 * @return the idHabilidade
+	 */
 	public ArrayList<Integer> getIdHabilidade() {
 		return idHabilidade;
 	}
 
-
+	/**
+	 * @param idHabilidade the idHabilidade to set
+	 */
 	public void setIdHabilidade(ArrayList<Integer> idHabilidade) {
 		this.idHabilidade = idHabilidade;
 	}
 
-
+	/**
+	 * @return the idCandidatos
+	 */
 	public ArrayList<Integer> getIdCandidatos() {
 		return idCandidatos;
 	}
 
-
+	/**
+	 * @param idCandidatos the idCandidatos to set
+	 */
 	public void setIdCandidatos(ArrayList<Integer> idCandidatos) {
 		this.idCandidatos = idCandidatos;
 	}
 
-
+	/**
+	 * @return the status
+	 */
 	public boolean isStatus() {
 		return status;
 	}
 
-
+	/**
+	 * @param status the status to set
+	 */
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
-	
-	
-
-	
-	
-
-	
-	
 
 }
