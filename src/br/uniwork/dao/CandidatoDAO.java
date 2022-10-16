973328ad1fd32932e17c67c711e7e8df4234b080
Mateus-Cabral <101184627+Mateus-Cabral@users.noreply.github.com>
2022-10-15 21:27:05 -0300

    Atualização do candidatoDAO

--- src/br/uniwork/dao/CandidatoDAO.java
+++ src/br/uniwork/dao/CandidatoDAO.java
@@ -8,61 +8,97 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.uniwork.model.Factory.ConectionFactory;
 import br.uniwork.model.vo.CandidatoVO;
-import oracle.jdbc.datasource.impl.OracleDataSource;
+/**
+ * Classe que gerencia a as operações de CRUD da classe Candidato
+ * @see #CandidatoVO()
+ * @see #CandidatoBO()
+ * @author Mateus Cabral
+ * @version 1.0
+ */
 
 public class CandidatoDAO {
 	
-	private String connString = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-	private Connection conn;
+	private Connection conn = null;
 	
 	public CandidatoDAO() throws SQLException {
-		OracleDataSource ods = new OracleDataSource();
-		ods.setURL(connString);
-		ods.setUser(Dados.USER);
-		ods.setPassword(Dados.PWD);
-		
-		conn = ods.getConnection();
+		this.conn = new ConectionFactory().getConn();
 	}
 	
-	public void insert(CandidatoVO c) throws SQLException {
+	/**
+	 * Insere um candidato no sistema, precisa de todos os campos
+	 * @param cvo
+	 * @throws SQLException
+	 */
+	public void insert(CandidatoVO cvo) throws SQLException {
 		String sql = "INSERT INTO T_UW_USUARIO "
-				+ "(nm_usuario, nr_rg, nr_cpf, nr_telefone, dt_nascimento, ds_email, "
-				+ " fl_genero, nm_user, nm_senha) VALUES "
+				+ "(nm_usuario, nr_rg, nr_cpf, nr_telefone, dt_nascimento, ds_email, fl_genero, nm_user, nm_senha) VALUES "
 				+ "(?,?,?,?,?,?,?,?,?,?)";
 		
 		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, c.getNome());
-		ps.setString(2, c.getRg());
-		ps.setInt(3, c.getCpf());
-		ps.setInt(4, Integer.parseInt(c.getCelular()));
-		ps.setDate(5, (Date) c.getDataNasc());
-		ps.setString(6, c.getEmail());
-		ps.setString(7, c.getGenero());
-		ps.setString(8, c.getLogin());
-		ps.setString(9, c.getPwd());
-		ps.execute();
+		ps.setString(1, cvo.getNome());
+		ps.setString(2, cvo.getRg());
+		ps.setInt(3, cvo.getCpf());
+		ps.setInt(4, Integer.parseInt(cvo.getCelular()));
+		ps.setDate(5, (Date) cvo.getDataNasc());
+		ps.setString(6, cvo.getEmail());
+		ps.setString(7, cvo.getGenero());
+		ps.setString(8, cvo.getLogin());
+		ps.setString(9, cvo.getPwd());
+		ps.executeUpdate();
+		ps.close();
+		conn.close();
 	}
 	
-	public CandidatoVO get(int id) throws SQLException {	
-		String sql = "SELECT FROM T_UW_USUARIO WHERE ID_USUARIO = ?";
+	
+	/**
+	 * Get by ID
+	 * Retorna um candidato do banco de dados partir do ID
+	 * @param id inteiro para busca
+	 * @return cvo CandidatoVO 
+	 * @throws SQLException
+	 */
+	public CandidatoVO select(int id) throws SQLException {	
+		String sql = "SELECT * FROM T_UW_USUARIO WHERE ID_USUARIO = (?)";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.setInt(1, id);
 		ResultSet rs = ps.executeQuery();
-		String nome = rs.getString(1);
-		String rg = rs.getString(2);
-		int cpf = rs.getInt(3);
-		int celular = rs.getInt(4);
-		Date dt_nascimento = rs.getDate(5);
-		String email = rs.getString(6);
-		String genero = rs.getString(7);
-		String login = rs.getString(8);
-		String senha = rs.getString(9);
-		CandidatoVO cv = new CandidatoVO(nome, id, login, senha, email, Integer.toString(celular), cpf, rg, genero, dt_nascimento);
-		return cv;
+		CandidatoVO cvo = new CandidatoVO();
+		while(rs.next()) {
+			String nome = rs.getString("nm_usuario");
+			String rg = rs.getString("nr_rg");
+			int cpf = rs.getInt("nr_cpf");
+			int celular = rs.getInt("nr_telefone");
+			Date dt_nascimento = rs.getDate("dt_nascimento");
+			String email = rs.getString("ds_email");
+			String genero = rs.getString("fl_genero");
+			String login = rs.getString("nm_user");
+			String senha = rs.getString("nm_senha");
+			cvo.setNome(nome);
+			cvo.setRg(rg);
+			cvo.setCpf(cpf);
+			cvo.setCelular(Integer.toString(celular));
+			cvo.setDataNasc(dt_nascimento);
+			cvo.setEmail(email);
+			cvo.setGenero(genero);
+			cvo.setLogin(login);
+			cvo.setPwd(senha);
+		}
+		ps.close();
+		rs.close();
+		conn.close();
+		
+		return cvo;
 	}
 	
-	public List<CandidatoVO> get() throws SQLException {
+	/**
+	 * Get all
+	 * Retorna todos os candidato do banco de dados
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<CandidatoVO> select() throws SQLException {
 		List<CandidatoVO> candidatos = new ArrayList<CandidatoVO>();
 		
 		String sql = "SELECT * FROM T_UW_USUARIO";
@@ -84,6 +120,9 @@ public class CandidatoDAO {
 			candidatos.add(new CandidatoVO(nome, id, login, senha, email, Integer.toString(celular), cpf, rg));
 			
 		}
+		ps.close();
+		rs.close();
+		conn.close();
 		
 		return candidatos;
 		
@@ -94,7 +133,9 @@ public class CandidatoDAO {
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.setString(1, novoNome);
 		ps.setInt(2, id);
-		ps.execute();
+		ps.executeUpdate();
+		ps.close();
+		conn.close();
 	}
 	
 	public void updatePwd(int id, String novaSenha) throws SQLException {
@@ -102,20 +143,22 @@ public class CandidatoDAO {
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.setString(1, novaSenha);
 		ps.setInt(2, id);
-		ps.execute();
+		ps.executeUpdate();
+		ps.close();
+		conn.close();
 	}
 	
 	
 	
 	public void delete(int id) throws SQLException {
-		String sql = "DELETE FROM T_UW_USUARIO WHERE ID = ?";
+		String sql = "DELETE FROM T_UW_USUARIO WHERE ID_USUARIO = ?";
 		PreparedStatement ps = conn.prepareStatement(sql);
 		ps.setInt(1, id);
 		ps.execute();
-	}
-	
-	public void encerrarConexao() throws SQLException {
+		ps.close();
 		conn.close();
 	}
+	
+
 
 }
